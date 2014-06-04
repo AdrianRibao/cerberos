3bba60c0bde8e0a9d2feee284804612dc46f97d8
Adrián Ribao Martínez <adrian@adrima.es>
2014-06-04 12:37:08 +0200

    adapted models to pep8

--- cerberos/models.py
+++ cerberos/models.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.contrib.sites.models import Site
 from django.db import models
-#from django.template.defaultfilters import slugify
-from django.utils.translation import ugettext_lazy as _ 
-#from django.core.urlresolvers import reverse
-#from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 from cerberos.settings import MEMORY_FOR_FAILED_LOGINS
-import datetime
 
 try:
     from django.utils import timezone
 except ImportError:
     from datetime import datetime as timezone
+
 
 class FailedAccessAttempt(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -19,15 +16,15 @@ class FailedAccessAttempt(models.Model):
     site = models.ForeignKey(Site, verbose_name=_(u'Site'))
     ip_address = models.IPAddressField(verbose_name=_(u'IP Address'), null=True, db_index=True)
     user_agent = models.CharField(max_length=255, verbose_name=_(u'User Agent'), blank=False,
-            help_text=_(u'User agent used in the login attempt'))
+                                  help_text=_(u'User agent used in the login attempt'))
     username = models.CharField(max_length=255, verbose_name=_(u'Username'), blank=False, db_index=True,
-            help_text=_(u'Username used to login'))
+                                help_text=_(u'Username used to login'))
     failed_logins = models.PositiveIntegerField(verbose_name=_(u'Failed logins'), default=0,
-            help_text=_(u'Failed logins for this IP'))
+                                                help_text=_(u'Failed logins for this IP'))
     locked = models.BooleanField(verbose_name=_(u'Locked'), default=False, db_index=True,
-            help_text=_(u'Indicates if the IP has been locked out.'))
-    expired = models.BooleanField(verbose_name=_(u'Expired'), default=False, 
-            help_text=_(u'Indicates if the timeout has expired.'))
+                                 help_text=_(u'Indicates if the IP has been locked out.'))
+    expired = models.BooleanField(verbose_name=_(u'Expired'), default=False,
+                                  help_text=_(u'Indicates if the timeout has expired.'))
 
     get_data = models.TextField('GET Data')
     post_data = models.TextField('POST Data')
@@ -37,9 +34,9 @@ class FailedAccessAttempt(models.Model):
     class Meta:
         verbose_name = _(u'Failed access attempt')
         verbose_name_plural = _(u'Failed access attempts')
-        
+
     def __unicode__(self):
-        return u'%s: %s' % (self.username, self.ip_address )
+        return u'%s: %s' % (self.username, self.ip_address)
 
     def get_time_to_forget(self):
         """
@@ -61,7 +58,7 @@ class FailedAccessAttempt(models.Model):
 
         if not self.locked:
             return _(u'Not locked yet')
-        elif time_remaining == None:
+        elif time_remaining is None:
             return _(u'Infinite')
         elif time_remaining <= 0:
             return _(u'Forgotten')
